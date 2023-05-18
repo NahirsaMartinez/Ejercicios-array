24b2313f3719f62889443c73db231796a2398a27
Nahir Martinez <nahirmartinez09@gmail.com>
2023-05-18 12:00:17 -0300

    ejercicio 2 terminado

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,7 +1,12 @@
 
 const numeros = [1,2,3,4,5,6,7];
-console.log(numeros[0]);
-console.log(numeros[3]);
-console.log(numeros[6]);
+const letras = ["a","b","c","d","e","f","g"];
+const colores = ["rojo","azul","amarillo","violeta","verde","gris","negro"];
 
-console.log(`El tipo de dato elegido ${numeros[1]} es numerico`);
+console.log(numeros[0],letras[0],colores[0]);
+console.log(numeros[3],letras[3],colores[3]);
+console.log(numeros[6],letras[6],colores[6]);
+
+console.log(`El tipo de dato elegido ${numeros[1]} es numerico.
+El tipo de dato elegido ${letras[1]} es un caracter.
+El tipo de dato elegido ${colores[1]} es una palabra `);
---
+++ ejercicio2.js
@@ -0,0 +1,4 @@
+const datos = ["violeta",32,"j"];
+datos[1] = 15;
+
+console.log(datos);
