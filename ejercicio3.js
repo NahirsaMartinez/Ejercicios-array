72c2ad15b1f29543254c1b127cdc057f9973f74c
Nahir Martinez <nahirmartinez09@gmail.com>
2023-05-18 12:13:36 -0300

    ejercicio 3 terminado

---
+++ ejercicio3.js
@@ -0,0 +1,9 @@
+const animales = ["foca","gato","perro"];
+const comidaIniciales = ["p", "n","m"];
+const numeros = [87,3,45];
+
+animales.splice(1,1);
+comidaIniciales.splice(1,1);
+numeros.splice (1,1);
+
+console.log(animales,comidaIniciales,numeros);
